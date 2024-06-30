c4d445a7806eb55dd77931efad54e44ed713d626
Oscar Gabriel Lezcano Ortellado <161733470+OscarLezcano@users.noreply.github.com>
2024-06-30 16:54:08 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,36 @@
-let intentos = 6;
-const VERDE = 'green';
-const AMARILLO = 'yellow';
-const BUTTON = document.getElementById("guess-button");
-//let diccionario = ['MANZANA','CASA','DAVID'];
-//let palabra = diccionario[Math.floor(Math.random() * diccionario.length)];
 let palabra;
+let intentos;
+const VERDE = '#144832';
+const PURPURA = '#471448';
+const BUTTON = document.getElementById("guess-button");
+
 
 async function initGame() {
     console.log('Juego Iniciado');
     palabra = await getPalabraAleatoria();
     palabra = palabra.toUpperCase(); 
+    intentos = palabra.length + 1;
     console.log(palabra);
+    Swal.fire({
+        title: "Importante",
+        text: "Posees " + intentos + " intentos, la palabra posee " + palabra.length + " letras",
+        width: 600,
+        icon: "info",
+        padding: "3em",
+        color: "#716add",
+        backdrop: "rgba(40, 55, 71,0.4)"
+      });
 }
 function validarIntento() {
     const INTENTO = getIntento();
     if (INTENTO.length !== palabra.length) {
-        alert("El intento debe tener " + palabra.length + " letras.");
+        Swal.fire({
+            position: "top-end",
+            icon: "warning",
+            title: "La palabra debe tener " + palabra.length + " letras",
+            showConfirmButton: false,
+            timer: 6000
+          });
         return;
     }
 
@@ -34,7 +49,7 @@ function validarIntento() {
         if (INTENTO[i] === palabra[i]) {
             SPAN.style.backgroundColor = VERDE;
         } else if (palabra.includes(INTENTO[i])) {
-            SPAN.style.backgroundColor = AMARILLO;
+            SPAN.style.backgroundColor = PURPURA;
         }
         SPAN.innerHTML = INTENTO[i];
         ROW.appendChild(SPAN);
@@ -43,7 +58,7 @@ function validarIntento() {
     GRID.appendChild(ROW);
     intentos--;
     if (intentos === 0) {
-        endGame("<h1>Perdiste. La palabra era: " + palabra + "</h1>");
+        endGame("<h1>Perdiste. La palabra era: " + palabra + " 🤦‍♂️"+"</h1>");
     }
 }
 
@@ -61,7 +76,7 @@ function getIntento() {
 
 async function getPalabraAleatoria() {
     try {
-        const response = await fetch('https://random-word-api.herokuapp.com/word?lang=es');
+        const response = await fetch("https://random-word-api.herokuapp.com/word?lang=es&length=5");
         const data = await response.json();
         return String(data[0]);
     } catch (error) {
